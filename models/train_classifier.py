cd0fd2b8105c5169da74d605824d7c10a64aaa9c
SJ_ZHAO <zhaoshj27@163.com>
2020-01-10 09:15:46 +0800

    add GridSearchCV in build_model()

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -11,6 +11,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 
 
@@ -53,15 +54,26 @@ def tokenize(text):
 
 def build_model():
     """
-    build pipeline
-    :return: (Pipeline)
+    Build pipeline and use grid search to find better parameters.
+    :return: (GridSearchCV)
     """
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
-    return pipeline
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
